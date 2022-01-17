816a938144023c3c552ba652d2588b33189aa0b3
Oleksandra9 <sashakrytska@gmail.com>
2022-01-17 18:33:37 +0000

    task2 +

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ulEl = document.querySelector("#ingredients");
+// console.log(ulEl);
+
+
+
+const elements = ingredients.map(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.classList.add("item");
+  liEl.textContent = `${ingredient}`;  
+  // console.log(liEl);
+  return liEl;
+  
+});
+console.log(elements);
+
+ulEl.append(...elements);
