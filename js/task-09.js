39e7b84c6f417459c00160e5bb1c2cc9e459fa6f
Oleksandra9 <sashakrytska@gmail.com>
2022-01-23 20:15:01 +0000

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+const refs = {
+  bodyEl : document.querySelector("body"),
+  spanEl : document.querySelector(".color"),
+  btnCgangeColorEl: document.querySelector(".change-color")
+}
+
+console.log(refs.btnCgangeColorEl);
+console.log(refs.spanEl);
+console.log(refs.bodyEl);
+
+refs.btnCgangeColorEl.addEventListener("click",onChangeColor);
+function onChangeColor () {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanEl.textContent = getRandomHexColor();
+}
