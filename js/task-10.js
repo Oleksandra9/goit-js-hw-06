f7f89f912393ac338ea3c6d6048c52be76b80747
Oleksandra9 <sashakrytska@gmail.com>
2022-01-23 23:48:10 +0000

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl : document.querySelector("input"),
+  btnCreate : document.querySelector(`[data-create]`),
+  btnDestroy : document.querySelector(`[data-destroy]`),
+  divBoxesEl : document.querySelector("#boxes")
+}
+
+refs.btnCreate.addEventListener("click",getAmount);
+
+function getAmount() {
+  let amount = Number(refs.inputEl.value);
+  onCreateBoxes(amount);
+};
+
+function onCreateBoxes (amount) {
+  let widthHeight = 30;
+    for (let i = 0; i < amount; i+=1) { 
+      widthHeight += 10;
+  const newDiv = document.createElement('div');
+  newDiv.style.width = `${widthHeight}px`;
+  newDiv.style.height = `${widthHeight}px`;
+  newDiv.style.background = getRandomHexColor();
+  
+  refs.divBoxesEl.append(newDiv); 
+};
+};
+
+refs.btnDestroy.addEventListener("click",onDestroy);
+function onDestroy () {
+  refs.divBoxesEl.innerHTML= " ";
+}
