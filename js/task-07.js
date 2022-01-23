31e7409d60f1350aa5587d33e8150d231a9a0fd4
Oleksandra9 <sashakrytska@gmail.com>
2022-01-23 19:11:33 +0000

    task7

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector("#font-size-control");
+const spanEl = document.querySelector("#text")
+
+
+inputEl.addEventListener("input",onInputChange);
+function onInputChange (event) {
+    
+    spanEl.style.fontSize = `${event.currentTarget.value}px`;
+    
+    spanEl.style.color = `green`;
+};
