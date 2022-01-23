e8bef10b1ce988b4baac9a668d59b58d085d7e05
Oleksandra9 <sashakrytska@gmail.com>
2022-01-23 17:34:32 +0000

    task5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+
+const inputEl = document.querySelector("#name-input");
+const spanNameEl = document.querySelector('#name-output');
+
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange (event) {
+   spanNameEl.classList.add("color");
+    if (event.currentTarget.value === "") {
+        spanNameEl.textContent = 'Anonymous';
+    }
+     spanNameEl.textContent = event.currentTarget.value;
+};
